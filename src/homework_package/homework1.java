8cb3aa18722a92c97b634bb935b84f8879c9b4af
alena <alena.evlan@gmail.com>
2023-03-05 16:25:30 +0200

    My homework 05.03.2023

---
+++ src/homework_package/homework1.java
@@ -0,0 +1,16 @@
+package homework_package;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class homework1 {
+    public static void main(String[] args){
+        Set<Integer> numbers = new HashSet<>();
+        numbers.add(56);
+        numbers.add(34);
+        numbers.add(84);
+        numbers.add(41);
+        numbers.add(34);
+        System.out.println(numbers);
+    }
+}
